38187f6a5ba2f14900430ab47b7cec8189a72775
yuho <unmet23@gmail.com>
2026-09-19 08:02:52 +0900

    Publish PULSE Jeju mascot play scene

--- sites.js
+++ sites.js
@@ -1,5 +1,5 @@
-import {initOrbConversation} from './orb-conversation.js?v=f08e492c196c';
-import {initGlassControls} from './glass-controls.js?v=f08e492c196c';
+import {initOrbConversation} from './orb-conversation.js?v=5d42af79e854';
+import {initGlassControls} from './glass-controls.js?v=5d42af79e854';
 initGlassControls();
 // A deliberate brand preview: swiping selects; following a link navigates.
 const brandDeck=document.querySelector('[data-brand-deck]');
