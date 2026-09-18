6108b1c8bf45e612c0ff8f311a8554813636477e
yuho <unmet23@gmail.com>
2026-09-19 06:21:46 +0900

    Refine company section spacing and closing paragraph

--- sites.js
+++ sites.js
@@ -1,4 +1,4 @@
-import {initOrbConversation} from './orb-conversation.js?v=25119a198b6d';
+import {initOrbConversation} from './orb-conversation.js?v=b4623101843e';
 // A deliberate brand preview: swiping selects; following a link navigates.
 const brandDeck=document.querySelector('[data-brand-deck]');
 if(brandDeck){
