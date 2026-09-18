37b7ecdfa0b779cbd43adbbf9829e06fb2da71eb
yuho <unmet23@gmail.com>
2026-09-19 08:21:52 +0900

    Publish balanced PATH practice layout

--- sites.js
+++ sites.js
@@ -1,5 +1,5 @@
-import {initOrbConversation} from './orb-conversation.js?v=5d42af79e854';
-import {initGlassControls} from './glass-controls.js?v=5d42af79e854';
+import {initOrbConversation} from './orb-conversation.js?v=69e4e32b730c';
+import {initGlassControls} from './glass-controls.js?v=69e4e32b730c';
 initGlassControls();
 // A deliberate brand preview: swiping selects; following a link navigates.
 const brandDeck=document.querySelector('[data-brand-deck]');
