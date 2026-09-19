5954b6a6d9889528dca22748603eb8893f0764f2
yuho <unmet23@gmail.com>
2026-09-19 21:56:53 +0900

    Publish PATH portfolio illustration

--- sites.js
+++ sites.js
@@ -1,5 +1,5 @@
-import {initOrbConversation} from './orb-conversation.js?v=928634a954c7';
-import {initGlassControls} from './glass-controls.js?v=928634a954c7';
+import {initOrbConversation} from './orb-conversation.js?v=971996ca8148';
+import {initGlassControls} from './glass-controls.js?v=971996ca8148';
 initGlassControls();
 // A deliberate brand preview: swiping selects; following a link navigates.
 const brandDeck=document.querySelector('[data-brand-deck]');
